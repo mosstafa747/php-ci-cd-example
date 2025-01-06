7292dbd6997300ba879b295fb82a2c5219026398
mosstafa <mosstafaaitalla@gmail.com>
2025-01-06 16:11:36 +0100

    connect with ..

---
+++ test.php
@@ -0,0 +1,12 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class IndexTest extends TestCase
+{
+    public function testOutput()
+    {
+        $output = "مرحبًا بك في مشروع التكامل المستمر CI/CD!";
+        $this->assertEquals($output, "مرحبًا بك في مشروع التكامل المستمر CI/CD!");
+    }
+}
+?>
